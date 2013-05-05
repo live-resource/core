59a6f6c58b8d3b1dffb69756106c2ec0997aa57f
Will Madden <w.a.madden@gmail.com>
2013-05-05 15:22:24 +0200

    Removed commented-out dependency code from Resource

--- lib/live_resource/resource.rb
+++ lib/live_resource/resource.rb
@@ -1,9 +1,5 @@
-require "live_resource/protocol"
-#require "live_resource/dependencies/active_record_dependency"
-
 module LiveResource
   class Resource
-    include Protocol
 
     attr_reader :name, :dependencies
 
@@ -16,30 +12,5 @@ module LiveResource
       raise "You must define an identifier method for the resource '#{@name}'"
     end
 
-
-
-    #def depends_on(target, events, proc)
-    #  raise "You must supply a proc for the dependency" if proc.nil?
-    #  @dependencies[target] << dependency = new_dependency(target, events, proc)
-    #  dependency.watch
-    #end
-    #
-    #def dependencies_on(target, events = nil)
-    #  if events
-    #    @dependencies[target].select { |d| d.events == events }
-    #  else
-    #    @dependencies[target]
-    #  end
-    #end
-    #
-    #def dependent_on?(target, events = nil)
-    #  not dependencies_on(target, events).empty?
-    #end
-    #
-    ## Initialize a new dependency on the target object.
-    ## The only supported targets are ActiveRecord model classes.
-    #def new_dependency(target, events, proc)
-    #  LiveResource::Dependencies::ActiveRecordDependency.new(self, target, events, proc)
-    #end
   end
 end
