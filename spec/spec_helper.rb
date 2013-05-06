fa88e42d1062bd5d25a7e30280b504f882b35fb4
Will Madden <w.a.madden@gmail.com>
2013-05-06 11:43:36 +0200

    Added support for expect_its(:attr) tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,12 +12,19 @@ end
 require_tree '../lib'
 require_tree 'support'
 
+module DescribeHelpers
+  def expect_its(*args, &block)
+    its(*args, &block)
+  end
+end
+
 RSpec.configure do |config|
   config.include LiveResourceMatchers
 
   # Alias the existing one-liner syntax to #expect_it, so that lines like the following will work:
   #   expect_it { to be(:something) }
   config.alias_example_to :expect_it
+  config.extend(DescribeHelpers)
 end
 
 RSpec::Core::ExampleGroup.module_eval do
