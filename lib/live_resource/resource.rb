949311ba05135640a975c152eabb23af9a34a322
Will Madden <w.a.madden@gmail.com>
2013-05-06 11:42:30 +0200

    Made the Resource accept a Protocol instance

--- lib/live_resource/resource.rb
+++ lib/live_resource/resource.rb
@@ -3,13 +3,18 @@ module LiveResource
 
     attr_reader :name, :dependencies
 
-    def initialize(name)
-      @name = name
+    def initialize(name, protocol)
+      @name         = name
+      @protocol     = protocol
       @dependencies = []
     end
 
     def identifier(*context)
       raise "You must define an identifier method for the resource '#{@name}'"
+    end
+
+    def push(*context)
+      @protocol.publish_resource_reset(identifier(*context))
     end
 
   end
--- spec/live_resource/resource_spec.rb
+++ spec/live_resource/resource_spec.rb
@@ -3,14 +3,47 @@ require "live_resource/resource"
 
 describe LiveResource::Resource do
 
-  let(:resource) { LiveResource::Resource.new(resource_name) }
+  let(:resource) { LiveResource::Resource.new(resource_name, protocol) }
   let(:resource_name) { "some_resource" }
+  let(:protocol) { double(LiveResource::Protocol) }
+
+  describe "#intialize" do
+    subject { resource }
+
+    its(:name) { should be resource_name }
+
+    it 'should retain its protocol' do
+      expect(subject.instance_variable_get(:@protocol)).to be(protocol)
+    end
+  end
 
   describe "#identifier" do
     subject { resource.identifier(:a, :b) }
 
     it "should raise an exception" do
       expect(lambda { subject }).to raise_exception
+    end
+  end
+
+  describe "#push" do
+    subject { resource.push(*context) }
+
+    let(:identifier) { 'a/b/c' }
+    let(:context) { [:a, :b, :c] }
+
+    before do
+      resource.stub(identifier: identifier)
+      protocol.stub(publish_resource_reset: nil)
+    end
+
+    it 'should generate the identifier' do
+      resource.should_receive(:identifier).with(*context)
+      subject
+    end
+
+    it 'should publish a resource reset' do
+      protocol.should_receive(:publish_resource_reset).with(identifier)
+      subject
     end
   end
 
